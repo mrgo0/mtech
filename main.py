4aeaf0b59b631d93955e22b79b961708cc745204
mrgo0 <115457805+mrgo0@users.noreply.github.com>
2023-12-19 13:54:46 +0300

    Update main.py

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ class Pyramid:
     def __init__(self, pyramid):
         self.pyramid = pyramid
 
+    
     def compress(self):
         return ''.join(sorted(set(self.pyramid)))
 
@@ -9,10 +10,8 @@ class Pyramid:
 def main():
     with open('input.txt', 'r') as file:
         data = file.read().replace('\n', '')
-
     pyramid = Pyramid(data)
     compressed_pyramid = pyramid.compress()
-
     with open('output.txt', 'w') as file:
         file.write(compressed_pyramid)
 
